0ff854594bd365442395f6b050b7bf6cfbe91188
trebektas <trensarbektas@gmail.com>
2023-08-06 23:24:31 +0200

    add some text to components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,17 @@
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      Footer - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+      pulvinar quam sed risus sollicitudin, nec tincidunt tortor egestas. Aenean
+      dictum justo id vulputate vehicula. Nunc in euismod nisl, in ornare justo.
+      Phasellus id mollis odio, imperdiet venenatis eros. Sed non est urna.
+      Vivamus non neque suscipit lectus mollis bibendum eget eget tortor.
+      Praesent porta, ligula a auctor fringilla, ipsum lorem placerat magna, in
+      eleifend quam felis nec leo. In mi quam, elementum at porttitor vitae,
+      eleifend vitae lacus. Praesent rutrum odio ac libero efficitur, cursus
+      consectetur ex porta. Praesent facilisis justo id mattis tempus.
+    </footer>
+  );
 };
 
 export default Footer;
