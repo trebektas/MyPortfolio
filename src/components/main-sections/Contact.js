6f450481e0db1cb55bc64f9e921bd2b4a1e7dfd2
trebektas <trensarbektas@gmail.com>
2023-08-08 20:25:17 +0200

    add more text

--- src/components/main-sections/Contact.js
+++ src/components/main-sections/Contact.js
@@ -9,7 +9,16 @@ const Contact = () => {
       Praesent porta, ligula a auctor fringilla, ipsum lorem placerat magna, in
       eleifend quam felis nec leo. In mi quam, elementum at porttitor vitae,
       eleifend vitae lacus. Praesent rutrum odio ac libero efficitur, cursus
-      consectetur ex porta. Praesent facilisis justo id mattis tempus.
+      consectetur ex porta. Praesent facilisis justo id mattis tempus. Lorem
+      ipsum dolor sit amet, consectetur adipiscing elit. Morbi pulvinar quam sed
+      risus sollicitudin, nec tincidunt tortor egestas. Aenean dictum justo id
+      vulputate vehicula. Nunc in euismod nisl, in ornare justo. Phasellus id
+      mollis odio, imperdiet venenatis eros. Sed non est urna. Vivamus non neque
+      suscipit lectus mollis bibendum eget eget tortor. Praesent porta, ligula a
+      auctor fringilla, ipsum lorem placerat magna, in eleifend quam felis nec
+      leo. In mi quam, elementum at porttitor vitae, eleifend vitae lacus.
+      Praesent rutrum odio ac libero efficitur, cursus consectetur ex porta.
+      Praesent facilisis justo id mattis tempus.
     </section>
   );
 };
