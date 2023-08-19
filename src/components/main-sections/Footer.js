5306503f2deb15fd3d71f806a43218cf5d57276f
trebektas <trensarbektas@gmail.com>
2023-08-19 22:26:25 +0200

    delete text from footer

--- src/components/main-sections/Footer.js
+++ src/components/main-sections/Footer.js
@@ -3,18 +3,7 @@ import "./footer.css";
 const Footer = () => {
   return (
     <section className="sections footer">
-      <div className="container-section">
-        Footer - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
-        pulvinar quam sed risus sollicitudin, nec tincidunt tortor egestas.
-        Aenean dictum justo id vulputate vehicula. Nunc in euismod nisl, in
-        ornare justo. Phasellus id mollis odio, imperdiet venenatis eros. Sed
-        non est urna. Vivamus non neque suscipit lectus mollis bibendum eget
-        eget tortor. Praesent porta, ligula a auctor fringilla, ipsum lorem
-        placerat magna, in eleifend quam felis nec leo. In mi quam, elementum at
-        porttitor vitae, eleifend vitae lacus. Praesent rutrum odio ac libero
-        efficitur, cursus consectetur ex porta. Praesent facilisis justo id
-        mattis tempus.
-      </div>
+      <div className="container-section">Footer Text</div>
     </section>
   );
 };
